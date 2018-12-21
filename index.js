d5d52ead5a82493011b221c2bd4ec913777ad705
Jhovahn Gibbs <nhavohj@gmail.com>
2018-12-20 21:41:13 -0800

    add regex

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/sms', (req, res) => {
 });
 
 app.post('/weather', (req, res) => {
-  let zip = Number(req.body);
+  let zip = req.body.replace(/[^0-9]+/g, '');
   console.log(req.body);
   let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=${WEATHER_API_KEY}&units=imperial`;
   return axios
