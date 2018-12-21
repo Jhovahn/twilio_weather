80298f139842cf16ecbfb8a7a9c5b4c377fdd3f0
Jhovahn Gibbs <nhavohj@gmail.com>
2018-12-21 12:56:01 -0800

    body changes

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/sms', (req, res) => {
 });
 
 app.post('/weather', (req, res) => {
-  let zip = req.body.toString(10);
+  let zip = req.body.Body;
   console.log(req.body);
   let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=${WEATHER_API_KEY}&units=imperial`;
   return axios
