c51fd25baa8ffa6a05463d9ae9e019758f43d062
Jhovahn Gibbs <nhavohj@gmail.com>
2018-12-20 22:05:55 -0800

    add twilio Body parameter

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/sms', (req, res) => {
 });
 
 app.post('/weather', (req, res) => {
-  let zip = 11221;
+  let zip = req.Body;
   console.log(req.body);
   let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=${WEATHER_API_KEY}&units=imperial`;
   return axios
